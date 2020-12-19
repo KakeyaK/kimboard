5d7a4effd988e68019f261d73f712a384053679c
kim <kimkakeya@gmail.com>
2020-12-18 22:59:32 -0300

    mudanças no código do servidor

--- projetoTeclado - Copia/servidor1.py
+++ projetoTeclado - Copia/servidor1.py
@@ -4,26 +4,29 @@ from pynput.keyboard import Key, Controller
 keyboard = Controller()
 
 
-meuHost = 'localhost'
+meuHost = ''
 minhaPort = 50007
 sockobj = socket(AF_INET, SOCK_STREAM)
 sockobj.bind((meuHost, minhaPort))
 sockobj.listen(1)
 
-ativo = True
+# ativo = True
 
-while ativo:
-    conexão, endereço = sockobj.accept()
-    print('Conectado por', endereço)
-    
-    while True:
-        data = conexão.recv(1024)
-        if data.decode() == "":
-            break
-        print("recebido:", data.decode( ))
-        #código para executar o que foi enviado
-        #keyboard.press(data.decode())
-        #keyboard.release(data.decode())
+# while ativo:
+print("vou procurar")
+conexão, endereço = sockobj.accept()
+print('Conectado por', endereço)
 
-    conexão.close()
-    break
+while True:
+    print("estou rodando")
+    data = conexão.recv(1024)
+    if data.decode() == "":
+        break
+    print("recebido:", data.decode( ))
+    tecla = data.decode()
+    print(tecla[1])
+    #código para executar o que foi enviado
+    keyboard.press(tecla[1])
+    keyboard.release(tecla[1])
+
+conexão.close()
